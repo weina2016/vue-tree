336504d83728dcc361dee524a327d9c32f046352
weina <na.wei@wudun.net>
2022-01-12 16:27:06 +0800

    tree init

--- src/main.js
+++ src/main.js
@@ -3,5 +3,12 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import "primevue/resources/themes/vela-purple/theme.css";
+import "primeicons/primeicons.css";
+import "primevue/resources/primevue.css";
+import "@/assets/css/global.css";
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <!--  <div class="home">-->
+  <!--    <img alt="Vue logo" src="../assets/logo.png" />-->
+  <!--    <HelloWorld msg="Welcome to Your Vue.js App" />-->
+  <!--  </div>-->
+  <ConditionGroup
+    :operator="conditionTree.operator"
+    :children="conditionTree.children"
+    :id="conditionTree.id"
+    @update:children="refreshChildren"
+  />
+  <div>{{ conditionTree }}</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import ConditionGroup from "@/components/ConditionGroup";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    ConditionGroup,
+  },
+  data() {
+    return {
+      conditionTree: {
+        id: 1,
+        kind: "group",
+        operator: "&",
+        children: [
+          { condition: "1==1", kind: "child", id: 1 },
+          {
+            id: 2,
+            kind: "group",
+            operator: "&",
+            children: [{ condition: "1==1", kind: "child", id: 3 }],
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    refreshChildren(newChildren) {
+      this.conditionTree.children = newChildren;
+    },
   },
 };
 </script>
